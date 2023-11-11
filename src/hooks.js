488f6e2783da63ff4b5b91fdc1378899e410edae
Bryce <bryce6425@gmail.com>
2023-11-10 22:41:13 -0500

    Added batch read LFG!!

--- src/hooks.js
+++ src/hooks.js
@@ -36,23 +36,19 @@ export function useLeague(userData) {
   const [rosters, setRosters] = useState(null);
   const [confirmedLeague, setConfirmedLeague] = useState(false);
 
+  useEffect(() => {
+    if (userData && userData.user_id) fetchUserLeagues(userData.user_id);
+  }, [userData]);
 
-    useEffect(() => {
-      if (userData && userData.user_id) fetchUserLeagues(userData.user_id);
-    }, [userData]);
-
-  
   useEffect(() => {
     if (confirmedLeague) fetchLeagueDetails(selectedLeague.league_id);
   }, [confirmedLeague]);
 
   const fetchUserLeagues = async (userId) => {
-    const response = await fetch(
-      `${API_BASE}/user/${userId}/leagues/nfl/2023`
-    );
+    const response = await fetch(`${API_BASE}/user/${userId}/leagues/nfl/2023`);
     const data = await response.json();
     setLeagues(data);
-  
+
     // Set the first league as the default selected league
     if (data && data.length > 0) {
       setSelectedLeague(data[0]);
@@ -64,7 +60,7 @@ export function useLeague(userData) {
       `${API_BASE}/league/${leagueId}/users`
     );
     const leagueUsers = await leagueUsersResponse.json();
-    
+
     const leagueTeamsResponse = await fetch(
       `${API_BASE}/league/${leagueId}/rosters`
     );
@@ -80,33 +76,33 @@ export function useLeague(userData) {
       return;
     }
     const teams = leagueUsers.map(async (userTeam) => {
-      const team = new Team(userTeam.display_name, userTeam.user_id, userTeam.avatar);
+      const team = new Team(
+        userTeam.display_name,
+        userTeam.user_id,
+        userTeam.avatar
+      );
       const roster = leagueTeams.find(
         (roster) => roster.owner_id === userTeam.user_id
       );
       let errorOccurred = false;
 
       if (!Array.isArray(roster.players)) {
-        console.error(`Error: roster for team ${team.teamName} is not an array`);
+        console.error(
+          `Error: roster for team ${team.teamName} is not an array`
+        );
         return;
       }
 
-      for (const newPlayer of roster.players) {
-
-        let player = await getPlayerData(newPlayer);
-        if (!player) {
-          console.log(`Error: player ${newPlayer} not found for team ${team.teamName}`);
-          errorOccurred = true;
-          break;
-        } else {
-          team.roster.push(player);
-        }
-      }
-
-      if (errorOccurred) {
-        console.error(`Error occurred while constructing team ${team.teamName}`);
+      try {
+        team.roster = await getDataForAllPlayers(roster.players);
+      } catch (ex) {
+        console.log(
+          `Error: Could not fetch players for team ${team.teamName}: `,
+          ex
+        );
         return;
       }
+
       team.calculateTotalPts();
       team.calculateTotalWeekly();
       team.calculateTradeValue();
@@ -128,60 +124,96 @@ export function useLeague(userData) {
     setConfirmedLeague(true);
   };
 
-  return { selectedLeague, handleLeagueChange, handleLeagueConfirm, rosters, leagues, confirmedLeague };
+  return {
+    selectedLeague,
+    handleLeagueChange,
+    handleLeagueConfirm,
+    rosters,
+    leagues,
+    confirmedLeague,
+  };
 }
 
-
-
 class Team {
-    constructor(tName, owner) {
-      this.teamName = tName || "";
-      this.owner_id = owner;
-      this.avatar_id = 0; 
-      this.roster = [];
-      this.totalPts = 0;
-      this.totalWeekly = 0;
-      this.totalTradeValue = 0;
-    }
-    construct(){
-      this.caclulateTotalPts();
-      this.calculateTradeValue();
-      this.calculateTotalWeekly();
-    }
-    calculateTotalPts() {
-      this.totalPts = 0;
-      this.roster.forEach((player) => {
-        this.totalPts += player.actualTotalPts;
-      });
-    }
-    calculateTradeValue(){
-      this.totalTradeValue = 0;
-      this.roster.forEach((player) => {
-        this.totalTradeValue += player.tradeValue;
-      });
-    }
-    calculateTotalWeekly(){
-      this.totalWeekly = 0;
-      this.roster.forEach((player) => {
-        this.totalWeekly += player.avgActualPts;
-      });
-    }
+  constructor(tName, owner) {
+    this.teamName = tName || "";
+    this.owner_id = owner;
+    this.avatar_id = 0;
+    this.roster = [];
+    this.totalPts = 0;
+    this.totalWeekly = 0;
+    this.totalTradeValue = 0;
   }
-
-
-  async function getPlayerData(playerId) {
-    try {
-      const response = await fetch(`http://localhost:8080/api/nfl/player/${playerId}`);
-      const playerData = await response.json();
-  
-      if (playerData.err) {
-        console.error(`Error from server when fetching player ${playerId}: `, playerData.err);
-        throw new Error(playerData.err);
-      } else {
-        return playerData;
-      }
-    } catch (error) {
-      console.error(`Error fetching data for player ${playerId}: `, error);
-      throw error;
-    }
+  construct() {
+    this.caclulateTotalPts();
+    this.calculateTradeValue();
+    this.calculateTotalWeekly();
   }
+  calculateTotalPts() {
+    this.totalPts = 0;
+    this.roster.forEach((player) => {
+      this.totalPts += player.actualTotalPts;
+    });
+  }
+  calculateTradeValue() {
+    this.totalTradeValue = 0;
+    this.roster.forEach((player) => {
+      this.totalTradeValue += player.tradeValue;
+    });
+  }
+  calculateTotalWeekly() {
+    this.totalWeekly = 0;
+    this.roster.forEach((player) => {
+      this.totalWeekly += player.avgActualPts;
+    });
+  }
+}
+
+async function getPlayerData(playerId) {
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/nfl/player/${playerId}`
+    );
+    const playerData = await response.json();
+
+    if (playerData.err) {
+      console.error(
+        `Error from server when fetching player ${playerId}: `,
+        playerData.err
+      );
+      throw new Error(playerData.err);
+    } else {
+      return playerData;
+    }
+  } catch (error) {
+    console.error(`Error fetching data for player ${playerId}: `, error);
+    throw error;
+  }
+}
+
+async function getDataForAllPlayers(players) {
+  try {
+    console.log(players);
+    const response = await fetch(`http://localhost:8080/api/nfl/player-batch`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(players),
+    });
+    const playerData = await response.json();
+
+    if (playerData.err) {
+      console.error(
+        `Error from server when fetching players: `,
+        playerData.err
+      );
+      throw new Error(playerData.err);
+    } else {
+      return playerData;
+    }
+  } catch (error) {
+    console.error(`Error fetching data for players: `, error);
+    throw error;
+  }
+}
